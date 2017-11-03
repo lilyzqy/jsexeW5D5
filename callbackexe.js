393c70b4557c3442ad0f0fcc6323c7a4cd7a72f4
lily <zq.yang.lily@gmail.com>
2017-11-03 16:44:47 -0700

    done exercises, begin towers of hanoi

--- callbackexe.js
+++ callbackexe.js
@@ -63,4 +63,77 @@ function addNumbers(sum, numsLeft, completionCallback) {
   }
 }
 
-addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
+// addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
+
+/// Absurd bubbleSort
+
+function askIfGreatThan(el1,el2,cb){
+  reader.question(`Is ${el1} bigger than ${el2}?`,function(answer){
+    if(answer.toLowerCase() === "yes"){
+      cb(true);
+    }else{
+      cb(false);
+    }
+  });
+}
+
+function innerBubbleSortLoop(arr,i, madeAnySwaps, outerBubbleSortLoop){
+
+  if(i < arr.length - 1){
+    askIfGreatThan(arr[i],arr[i + 1], (isGreaterThan)=>{
+      if (isGreaterThan) {
+        let stored = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = stored;
+        madeAnySwaps = true;
+      }
+      innerBubbleSortLoop(arr, i + 1, madeAnySwaps,outerBubbleSortLoop);
+    });
+  } else {
+    outerBubbleSortLoop(madeAnySwaps);
+  }
+}
+
+function absurdBubbleSort(arr, sortCompletionCallback){
+  function outerBubbleSortLoop(madeAnySwaps){
+    if (madeAnySwaps){
+      innerBubbleSortLoop(arr,0, false, outerBubbleSortLoop);
+    }else{
+      sortCompletionCallback(arr);
+      reader.close();
+    }
+  }
+  outerBubbleSortLoop(true);
+}
+
+// absurdBubbleSort([3, 2, 1], function (arr) {
+//   console.log("Sorted array: " + JSON.stringify(arr));
+//   reader.close();
+// });
+
+//myBind
+Function.prototype.myBind = function myBind(context){
+  return ()=>{
+    this.apply(context);
+  };
+};
+
+class Lamp {
+  constructor() {
+    this.name = "a lamp";
+  }
+}
+
+const turnOn = function() {
+   console.log(`Turning on ${this.name}`);
+};
+
+const lamp = new Lamp();
+
+turnOn(); // should not work the way we want it to
+
+const boundTurnOn = turnOn.bind(lamp);
+const myBoundTurnOn = turnOn.myBind(lamp);
+
+boundTurnOn(); // should say "Turning on a lamp"
+myBoundTurnOn(); // should say "Turning on a lamp"
