1eba8c3a8b1bd82c8be3a22e39f38b73dd17d238
lily <zq.yang.lily@gmail.com>
2017-11-03 18:07:35 -0700

    done isvalidmove and promptMove

--- Towers of Hanoi.js
+++ Towers of Hanoi.js
@@ -1,5 +1,36 @@
 class Game{
   constructor(){
-    
+    this.stacks = new Array;
   }
+
+  promptMove(cb){
+    console.log(this.stacks);
+    reader.question("Move from which stack?",(firstanswer)=>{
+      this.fromStackIdx = parseInt(firstanswer,10);
+      reader.question("Move to which stack?",(secondanswer)=>{
+        this.toStackIdx = parseInt(secondanswer,10);
+        cb(firstanswer,secondanswer);
+      });
+    });
+  }
+
+  isValidMove(fromStackIdx, toStackIdx) {
+    const fromStack = this.stacks[fromStackIdx];
+    const toStack = this.stacks[toStackIdx];
+    if (fromStack[fromStack.length - 1] > toStack[toStack.length - 1]) {
+      return false;
+    } else if (fromStack.length === 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
 }
+
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
